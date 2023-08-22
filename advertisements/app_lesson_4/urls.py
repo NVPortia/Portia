a0d39c4229102574302144960edf6f1baa45c26a
NVPortia <136724229+NVPortia@users.noreply.github.com>
2023-08-22 12:06:46 +0700

    Delete advertisements directory

--- advertisements/app_lesson_4/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index, top_sellers
-from django.conf import settings
-
-urlpatterns = [
-    path('', index, name='main-page'),
-    path('top-sellers/', top_sellers, name='top-sellers')
-]
